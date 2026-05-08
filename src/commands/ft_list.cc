f79ea9e7cc33ae08d0f458d59401962b1cc6f9d3
Aksha Thakkar <thaakb@amazon.com>
2026-05-08 09:59:36 -0700

    Add optional regex filter to FT._LIST command
    When managing many indexes, filtering the list by pattern is useful.
    Add an optional regex argument to FT._LIST that filters returned
    index names using ECMAScript regex (case-insensitive).
    
    Usage:
      FT._LIST            - returns all indexes (unchanged behavior)
      FT._LIST "prod.*"   - returns only indexes matching the pattern
    
    Fixes: https://github.com/valkey-io/valkey-search/issues/606
    Signed-off-by: Aksha Thakkar <thaakb@amazon.com>

--- src/commands/ft_list.cc
+++ src/commands/ft_list.cc
@@ -5,13 +5,16 @@
  *
  */
 
+#include <regex>
 #include <string>
 
 #include "absl/container/flat_hash_set.h"
 #include "absl/status/status.h"
+#include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
 #include "src/commands/commands.h"
 #include "src/schema_manager.h"
+#include "vmsdk/src/type_conversions.h"
 #include "vmsdk/src/utils.h"
 #include "vmsdk/src/valkey_module_api/valkey_module.h"
 
@@ -19,14 +22,34 @@ namespace valkey_search {
 
 absl::Status FTListCmd(ValkeyModuleCtx *ctx, ValkeyModuleString **argv,
                        int argc) {
-  if (argc > 1) {
+  if (argc > 2) {
     return absl::InvalidArgumentError(vmsdk::WrongArity(kListCommand));
   }
   absl::flat_hash_set<std::string> names =
       SchemaManager::Instance().GetIndexSchemasInDB(
           ValkeyModule_GetSelectedDb(ctx));
-  ValkeyModule_ReplyWithArray(ctx, names.size());
+
+  std::optional<std::regex> pattern;
+  if (argc == 2) {
+    auto pattern_str = vmsdk::ToStringView(argv[1]);
+    try {
+      pattern.emplace(std::string(pattern_str),
+                      std::regex::ECMAScript | std::regex::icase);
+    } catch (const std::regex_error &e) {
+      return absl::InvalidArgumentError(
+          absl::StrCat("Invalid regex pattern: ", e.what()));
+    }
+  }
+
+  std::vector<std::string> filtered_names;
   for (const auto &name : names) {
+    if (!pattern || std::regex_search(name, *pattern)) {
+      filtered_names.push_back(name);
+    }
+  }
+
+  ValkeyModule_ReplyWithArray(ctx, filtered_names.size());
+  for (const auto &name : filtered_names) {
     ValkeyModule_ReplyWithSimpleString(ctx, name.c_str());
   }
   return absl::OkStatus();
